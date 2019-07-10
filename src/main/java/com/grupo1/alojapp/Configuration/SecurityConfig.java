5b5bfe3fba82298c10250d9e3e2e646233327c5c
juanmanuelvallejos <juanmanuelvallejos2@gmail.com>
2019-07-09 22:02:36 -0300

    Se le quitan permisos de eliminacion de alojamientos a los usuarios que no sean ADMIN

--- src/main/java/com/grupo1/alojapp/Configuration/SecurityConfig.java
+++ src/main/java/com/grupo1/alojapp/Configuration/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    };
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -45,10 +45,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/alojamiento/**").hasRole("USER")
-                .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
-                .and().sessionManagement();
+            .authorizeRequests()
+            .antMatchers("/alojamiento/delete/**").hasRole("ADMIN")
+            .antMatchers("/alojamiento/**").hasAnyRole("USER","ADMIN")
+            .antMatchers("/usuario/**").hasAnyRole("USER","ADMIN")
+            .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+            .and().sessionManagement();
     }
 
     @Bean
