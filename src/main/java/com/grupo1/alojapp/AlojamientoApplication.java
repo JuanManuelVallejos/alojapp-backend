1d23acbd3b31902480542da0cefebd312fd18bbf
juanmanuelvallejos <juanmanuelvallejos2@gmail.com>
2019-07-16 21:48:14 -0300

    Se sube funcionalidad de carga y descarga de archivos funcionando localmente

--- src/main/java/com/grupo1/alojapp/AlojamientoApplication.java
+++ src/main/java/com/grupo1/alojapp/AlojamientoApplication.java
@@ -1,12 +1,17 @@
 package com.grupo1.alojapp;
 
+import com.grupo1.alojapp.Configuration.FileStorageProperties;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class AlojamientoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/grupo1/alojapp/Exceptions/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.grupo1.alojapp.Exceptions;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
